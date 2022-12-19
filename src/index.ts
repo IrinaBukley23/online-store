2eadbedef40226b10326d1af104ef53be33980ef
Irina <irenserge@mail.ru>
2022-12-19 14:50:03 +0300

    fix: fix игпы

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { appModule } from './app/app.module';
 import { bootstrap, wfm } from './routes';
 
-import 'bootstrap';
-import './scss/style.scss';
+// import 'bootstrap';
+// import './style.scss';
 
 wfm.delay(2000).then(() => {
     bootstrap(appModule);
