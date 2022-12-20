0a98bcd754f7344be5ca7578c09ba7495a0d0fcf
Olga Dementey <dementeyolya@gmail.com>
2022-12-20 15:02:18 +0300

    feat: products component

--- src/app/components/products/products.ts
+++ src/app/components/products/products.ts
@@ -10,22 +10,31 @@ class Products extends WFMComponent {
     }
 }
 
-const cardTemplate = `
-  <div class="card__title">
-  </div>
-  <div class="card__image">
-  </div>
-  <div class="card__description">
-  </div>
-  <div class="card__buttons">
-    <div class="button">Add to cart</div>
-    <div class="button">Details</div>
-  </div>
+let cardsTemplate = ``;
+
+productsData.products.forEach((product) => {
+    cardsTemplate += `
+        <div class="card col-3">
+          <div class="card__title">
+            ${product.title}
+          </div>
+          <div class="card__image">
+            <img src="${product.thumbnail}">
+          </div>
+          <div class="card__description">
+            ${product.description}
+          </div>
+          <div class="card__buttons">
+            <div class="button">Add to cart</div>
+            <div class="button">Details</div>
+          </div>
+        </div>   
 `;
+});
 
 export const products = new Products({
     selector: 'products',
     template: `
-      ${cardTemplate}
+      ${cardsTemplate}
     `,
 });
