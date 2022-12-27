161d3610534ffaf03138a4c8a9bcad7fa4909018
Olga Dementey <dementeyolya@gmail.com>
2022-12-27 20:29:27 +0300

    fix: part of  typescript errors

--- src/app/components/filter/dualSlider/dualSlider.ts
+++ src/app/components/filter/dualSlider/dualSlider.ts
@@ -8,7 +8,7 @@ class DualSlider extends WFMComponent {
     }
 
     init() {
-        function controlFromInput(fromSlider, fromInput, toInput, controlSlider) {
+        function controlFromInput(fromSlider, fromInput, toInput, controlSlider): void {
             const [from, to] = getParsed(fromInput, toInput);
             fillSlider(fromInput, toInput, '#C6C6C6', '#25daa5', controlSlider);
             if (from > to) {
@@ -19,7 +19,7 @@ class DualSlider extends WFMComponent {
             }
         }
 
-        function controlToInput(toSlider, fromInput, toInput, controlSlider) {
+        function controlToInput(toSlider: HTMLInputElement, fromInput: HTMLInputElement, toInput: HTMLInputElement, controlSlider: HTMLInputElement) {
             const [from, to] = getParsed(fromInput, toInput);
             fillSlider(fromInput, toInput, '#C6C6C6', '#25daa5', controlSlider);
             setToggleAccessible(toInput);
@@ -55,39 +55,39 @@ class DualSlider extends WFMComponent {
             }
         }
 
-        function getParsed(currentFrom, currentTo) {
+        function getParsed(currentFrom: HTMLInputElement, currentTo: HTMLInputElement) {
             const from = parseInt(currentFrom.value, 10);
             const to = parseInt(currentTo.value, 10);
             return [from, to];
         }
 
-        function fillSlider(from, to, sliderColor, rangeColor, controlSlider) {
-            const rangeDistance = to.max-to.min;
-            const fromPosition = from.value - to.min;
-            const toPosition = to.value - to.min;
+        function fillSlider(from: HTMLInputElement, to: HTMLInputElement, sliderColor: string, rangeColor: string, controlSlider: HTMLInputElement) {
+            const rangeDistance = +to.max - +to.min;
+            const fromPosition = +from.value - +to.min;
+            const toPosition = +to.value - +to.min;
             controlSlider.style.background = `linear-gradient(
-      to right,
-      ${sliderColor} 0%,
-      ${sliderColor} ${(fromPosition)/(rangeDistance)*100}%,
-      ${rangeColor} ${((fromPosition)/(rangeDistance))*100}%,
-      ${rangeColor} ${(toPosition)/(rangeDistance)*100}%, 
-      ${sliderColor} ${(toPosition)/(rangeDistance)*100}%, 
-      ${sliderColor} 100%)`;
-        }
+                  to right,
+                  ${sliderColor} 0%,
+                  ${sliderColor} ${(fromPosition)/(rangeDistance)*100}%,
+                  ${rangeColor} ${((fromPosition)/(rangeDistance))*100}%,
+                  ${rangeColor} ${(toPosition)/(rangeDistance)*100}%, 
+                  ${sliderColor} ${(toPosition)/(rangeDistance)*100}%, 
+                  ${sliderColor} 100%)`;
+            }
 
-        function setToggleAccessible(currentTarget) {
-            const toSlider = document.querySelector('#toSlider');
+        function setToggleAccessible(currentTarget: HTMLInputElement): void {
+            const toSlider = document.querySelector('#toSlider') as HTMLInputElement;
             if (Number(currentTarget.value) <= 0 ) {
-                toSlider.style.zIndex = 2;
+                toSlider.style.zIndex = '2';
             } else {
-                toSlider.style.zIndex = 0;
+                toSlider.style.zIndex = '0';
             }
         }
 
-        const fromSlider = document.querySelector('#fromSlider');
-        const toSlider = document.querySelector('#toSlider');
-        const fromInput = document.querySelector('#fromInput');
-        const toInput = document.querySelector('#toInput');
+        const fromSlider = document.querySelector('#fromSlider') as HTMLInputElement;
+        const toSlider = document.querySelector('#toSlider') as HTMLInputElement;
+        const fromInput = document.querySelector('#fromInput') as HTMLInputElement;
+        const toInput = document.querySelector('#toInput') as HTMLInputElement;
         fillSlider(fromSlider, toSlider, '#C6C6C6', '#25daa5', toSlider);
         setToggleAccessible(toSlider);
 
