7658e2b984db0c5d67e1c21470ad1c4ad830b54a
Irina <irenserge@mail.ru>
2022-12-30 17:59:24 +0300

    feat: add total counter

--- src/app/pages/cartPage/cartPage.ts
+++ src/app/pages/cartPage/cartPage.ts
@@ -1,6 +1,5 @@
 import { WFMComponent } from '../../../routes/index';
 import { CartItem, ComponentConfig } from '../../../types';
-import img from '../../../../public/cart.png';
 
 import './cartPage.scss';
 
@@ -8,46 +7,53 @@ class CartPage extends WFMComponent {
     constructor(config: ComponentConfig) {
         super(config);
     }
-}
 
-const cartArr: CartItem[] = JSON.parse(localStorage.getItem('cart') as string)
-console.log(cartArr);
-let cartTemplate = ``;
-let index = 1;
-cartArr.forEach((item: CartItem) => {
-    cartTemplate += `
-    <div class="cart__products-elem">
-        <p class="cart__products-num">${index++}</p>
-        <div class="cart__products-img">
-            <img src=${item.product.images[0]}> 
-        </div>
-        <div  class="cart__products-description">
-            <h3 class="cart__products-title">${item.product.title}</h3>
-            <p class="cart__products-descr"> ${item.product.description}</p>
-            <div class="cart__products-raiting">
-                <p>Rating: <span>${item.product.rating}</span> </p>
-                <p>Discount: <span>${item.product.discountPercentage}</span>% </p>
-            </div>
-        </div>
-        <div class="cart__products-sum">
-            <p>Stock: <span>${item.product.stock}</span> </p>
-            <div class="cart__counter">
-                <button> - </button> 
-                <span>${item.counter}</span> 
-                <button> + </button>
-            </div>
-            <p>€<span>${item.product.price}</span></p>
-        </div>
-    </div>         
-`
-})
+
+}
 
 export const cartPage = new CartPage({
     selector: 'cart',
     innerComponents: null,
     getTemplate() {
-        return `
-        <section class="cart">
+        let cartTemplate = ``;
+        let index = 1;
+        const cartArr: CartItem[] = JSON.parse(localStorage.getItem('cart') as string);
+        let cartTotalCounter = 0;
+        cartArr.forEach(item => {
+            cartTotalCounter += item.counter
+        })
+
+        cartArr.forEach((item: CartItem) => {
+            cartTemplate += `
+            <div class="cart__products-elem">
+                <p class="cart__products-num">${index++}</p>
+                <div class="cart__products-img">
+                    <img src=${item.product.images[0]}> 
+                </div>
+                <div  class="cart__products-description">
+                    <h3 class="cart__products-title">${item.product.title}</h3>
+                    <p class="cart__products-descr"> ${item.product.description}</p>
+                    <div class="cart__products-raiting">
+                        <p>Rating: <span>${item.product.rating}</span> </p>
+                        <p>Discount: <span>${item.product.discountPercentage}</span>% </p>
+                    </div>
+                </div>
+                <div class="cart__products-sum">
+                    <p>Stock: <span>${item.product.stock}</span> </p>
+                    <div class="cart__counter">
+                        <button> - </button> 
+                        <span>${item.counter}</span> 
+                        <button> + </button>
+                    </div>
+                    <p>€<span>${item.product.price}</span></p>
+                </div>
+            </div>`;
+        });
+
+        if(cartArr.length === 0) {
+            return `<h3>Cart is empty</h3>`
+        }
+        return `<section class="cart">
         <div class="container">
             <div class="cart__products">
                 <div class="cart__title-block">
@@ -65,15 +71,16 @@ export const cartPage = new CartPage({
                 ${cartTemplate}
                 </div>
             </div>
-                <div class="cart__summary">
-                    <h3 class="cart__summary-title">Summary</h3>
-                    <h4 class="cart__summary-subtitle">Products: <span></span> </h4>
-                    <h4 class="cart__summary-subtitle">Total: <span></span> </h4>
-                    <div class="cart__summary-promo">
-                        <input type="text" placeholder="Enter promo code">
-                        <label>Promo for test: 'RS', 'EPM'</label>
-                    </div>
-                    <button class="button">Buy now</button>
+            <div class="cart__summary">
+                <h3 class="cart__summary-title">Summary</h3>
+                <h4 class="cart__summary-subtitle">Products: <span>${cartTotalCounter}</span> </h4>
+                <h4 class="cart__summary-subtitle">Total: <span></span> </h4>
+                <div class="cart__summary-promo">
+                    <input type="text" placeholder="Enter promo code">
+                    <label>Promo for test: 'RS', 'EPM'</label>
                 </div>
-        </section>`}
+                <button class="button">Buy now</button>
+            </div>
+        </section>`
+    }
 });
